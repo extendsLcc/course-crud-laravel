115838ade804a8e9f079be095f5c21acd2399492
extendsLcc <lucas.lcc@hotmail.com>
2024-03-24 19:17:48 -0300

    fix(home): fix inconsistency of container position spacing with other pages
    Signed-off-by: extendsLcc <lucas.lcc@hotmail.com>

--- resources/views/HomePage.vue
+++ resources/views/HomePage.vue
@@ -8,16 +8,16 @@ const props = defineProps<App.Data.HomePageOutputData>()
 </script>
 
 <template layout="panel-shell">
-  <div class="flex flex-col">
-    <div class="flex-1 space-y-4 p-8 pt-6">
-      <div class="flex items-center justify-between space-y-2">
-        <h2 class="text-3xl font-bold tracking-tight">
-          Dashboard
-        </h2>
+  <div class="space-y-4 p-8 pt-4">
+    <div class="flex flex-col">
+      <div class="flex-1 space-y-4">
+        <div class="flex items-center justify-between space-y-2">
+          <h2 class="text-3xl font-bold tracking-tight">
+            Dashboard
+          </h2>
+        </div>
       </div>
     </div>
-  </div>
-  <div class="space-y-4">
     <div class="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
       <Card>
         <CardHeader class="flex flex-row items-center justify-between space-y-0 pb-2">
